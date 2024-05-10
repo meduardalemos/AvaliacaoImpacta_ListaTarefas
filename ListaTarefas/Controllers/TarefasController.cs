4358ed86beecc92bd5a56b25f4397510213dfd78
meduardalemos <maria.eduarda.lemos@hotmail.com>
2024-05-10 11:26:48 -0300

    Adicionar arquivos de projeto.

---
+++ ListaTarefas/Controllers/TarefasController.cs
@@ -0,0 +1,51 @@
+﻿using ListaTarefas.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaTarefas.Controllers
+{
+    public class TarefasController : Controller
+    {
+        private List<Tarefa> _tarefas = new List<Tarefa>
+        {
+            new Tarefa("Responder questionário"),
+            new Tarefa("Entregar projeto de lista de tarefas"),
+            new Tarefa("1:1 com professor")
+        };
+
+        public IActionResult Index()
+        {
+            return View(_tarefas);
+        }
+
+        public IActionResult Create()
+        {
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Concluidas() 
+        {
+            return View();
+        }
+
+        public IActionResult PorFazer()
+        {
+            return View();
+        }
+
+        public IActionResult Concluir()
+        {
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            if (tarefa == null)
+                return NotFound();
+            _tarefas.Remove(tarefa);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ListaTarefas/Models/Tarefa.cs
@@ -0,0 +1,30 @@
+﻿namespace ListaTarefas.Models
+{
+    public class Tarefa
+    {
+        private static int _idCounter = 1;
+        public int TarefaId { get; set; }
+        public string Nome { get; set; }
+        public Boolean Concluida { get; set; }
+        public DateTime DtInicio { get; set; }
+        public DateTime? DtConclusao { get; set; }
+
+        public Tarefa(string nome)
+        {
+            Nome = nome;
+            TarefaId = _idCounter++;
+            DtInicio = DateTime.Now;
+            Concluida = false;
+            DtConclusao = null;
+        }
+
+        public void ConcluirTarefa()
+        {
+            if (!Concluida)
+            {
+                Concluida = true;
+                DtConclusao = DateTime.Now;
+            }
+        }
+    }    
+}
