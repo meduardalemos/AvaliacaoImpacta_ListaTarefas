430f6e6e52714826153a27bbf93cb650f73a4859
meduardalemos <maria.eduarda.lemos@hotmail.com>
2024-05-10 13:30:04 -0300

    add create de Tarefa

--- ListaTarefas/Controllers/TarefasController.cs
+++ ListaTarefas/Controllers/TarefasController.cs
@@ -5,11 +5,11 @@ namespace ListaTarefas.Controllers
 {
     public class TarefasController : Controller
     {
-        private List<Tarefa> _tarefas = new List<Tarefa>
+        private static List<Tarefa> _tarefas = new List<Tarefa>
         {
-            new Tarefa("Responder questionário"),
-            new Tarefa("Entregar projeto de lista de tarefas"),
-            new Tarefa("1:1 com professor")
+            new Tarefa {TarefaId = 1, Nome = "Responder questionário"},
+            new Tarefa {TarefaId = 2, Nome = "Entregar projeto de lista de tarefas"},
+            new Tarefa {TarefaId = 3, Nome = "1:1 com professor para feedback"},
         };
 
         public IActionResult Index()
@@ -17,9 +17,34 @@ namespace ListaTarefas.Controllers
             return View(_tarefas);
         }
 
-        public IActionResult Delete()
+        public IActionResult Delete(int id)
+        {
+            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+            if (tarefa == null)
+                return NotFound();
+            _tarefas.Remove(tarefa);
+            return RedirectToAction("Index");
+        }
+
+        // Chama o form de cadastro
+        [HttpGet]
+        public IActionResult Create()
         {
             return View();
         }
+
+        // Recebe os dados do form
+        [HttpPost]
+        public IActionResult Create(Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                tarefa.TarefaId = _tarefas.Count() > 0 ? _tarefas.Max(t => t.TarefaId + 1) : 1;
+                _tarefas.Add(tarefa);
+            }
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
--- ListaTarefas/Models/Tarefa.cs
+++ ListaTarefas/Models/Tarefa.cs
@@ -2,29 +2,11 @@
 {
     public class Tarefa
     {
-        private static int _idCounter = 1;
         public int TarefaId { get; set; }
         public string Nome { get; set; }
-        public Boolean Concluida { get; set; }
-        public DateTime DtInicio { get; set; }
+        public Boolean Concluida { get; set; } = false;
+        public DateTime DtInicio { get; set; } = DateTime.Now;
         public DateTime? DtConclusao { get; set; }
 
-        public Tarefa(string nome)
-        {
-            Nome = nome;
-            TarefaId = _idCounter++;
-            DtInicio = DateTime.Now;
-            Concluida = false;
-            DtConclusao = null;
-        }
-
-        public void ConcluirTarefa()
-        {
-            if (!Concluida)
-            {
-                Concluida = true;
-                DtConclusao = DateTime.Now;
-            }
-        }
     }    
 }
