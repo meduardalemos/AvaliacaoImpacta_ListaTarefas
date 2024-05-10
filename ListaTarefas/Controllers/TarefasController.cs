382098f565786291944c3769b2f94e8528a8677e
meduardalemos <maria.eduarda.lemos@hotmail.com>
2024-05-10 14:56:36 -0300

    add views concluidas e por fazer

--- ListaTarefas/Controllers/TarefasController.cs
+++ ListaTarefas/Controllers/TarefasController.cs
@@ -1,50 +1,88 @@
 ﻿using ListaTarefas.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace ListaTarefas.Controllers
+namespace ListaTarefas.Controllers;
+
+public class TarefasController : Controller
 {
-    public class TarefasController : Controller
+    private static List<Tarefa> _tarefas = new List<Tarefa>
     {
-        private static List<Tarefa> _tarefas = new List<Tarefa>
-        {
-            new Tarefa {TarefaId = 1, Nome = "Responder questionário"},
-            new Tarefa {TarefaId = 2, Nome = "Entregar projeto de lista de tarefas"},
-            new Tarefa {TarefaId = 3, Nome = "1:1 com professor para feedback"},
-        };
+        new Tarefa {TarefaId = 1, Nome = "Estudar projetos MVC", DtInicio = DateTime.Parse("08/05/2024"),Concluida = true, DtConclusao = DateTime.Parse("09/05/2024")},
+        new Tarefa {TarefaId = 2, Nome = "Estudar Azure", DtInicio = DateTime.Parse("09/05/2024"),  Concluida = true, DtConclusao = DateTime.Parse("10/05/2024")},
+        new Tarefa {TarefaId = 3, Nome = "Responder questionário"},
+        new Tarefa {TarefaId = 4, Nome = "Entregar projeto de lista de tarefas"},
+        new Tarefa {TarefaId = 5, Nome = "1:1 com professor para feedback"}
+    };
 
-        public IActionResult Index()
+    public IActionResult Index()
+    {
+        return View(_tarefas);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+        if (tarefa == null)
+            return NotFound();
+        _tarefas.Remove(tarefa);
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Tarefa tarefa)
+    {
+        if (ModelState.IsValid)
         {
-            return View(_tarefas);
+            tarefa.TarefaId = _tarefas.Count() > 0 ? _tarefas.Max(t => t.TarefaId + 1) : 1;
+            _tarefas.Add(tarefa);
+        }
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Edit(int id)
+    {
+        var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
+        if (tarefa == null)
+        {
+            return NotFound();
         }
 
-        public IActionResult Delete(int id)
-        {
-            var tarefa = _tarefas.FirstOrDefault(t => t.TarefaId == id);
-            if (tarefa == null)
-                return NotFound();
-            _tarefas.Remove(tarefa);
-            return RedirectToAction("Index");
-        }
+        return View(tarefa);
+    }
 
-        // Chama o form de cadastro
-        [HttpGet]
-        public IActionResult Create()
+    [HttpPost]
+    public IActionResult Edit(Tarefa tarefa)
+    {
+        if (ModelState.IsValid)
         {
-            return View();
-        }
-
-        // Recebe os dados do form
-        [HttpPost]
-        public IActionResult Create(Tarefa tarefa)
-        {
-            if (ModelState.IsValid)
+            var existingTarefa = _tarefas.FirstOrDefault(t => t.TarefaId == tarefa.TarefaId);
+            if (existingTarefa != null)
             {
-                tarefa.TarefaId = _tarefas.Count() > 0 ? _tarefas.Max(t => t.TarefaId + 1) : 1;
-                _tarefas.Add(tarefa);
+                existingTarefa.Nome = tarefa.Nome;
             }
             return RedirectToAction("Index");
         }
-
-
+        return View(tarefa);
     }
+
+    public IActionResult Concluidas()
+    {
+        var _tarefasConcluidas = _tarefas.Where(t => t.Concluida == true).ToList();
+
+        return View(_tarefasConcluidas);
+    }
+
+    public IActionResult PorFazer()
+    {
+        var _tarefasPorFazer = _tarefas.Where(t => t.Concluida == false).ToList();
+
+        return View(_tarefasPorFazer);
+    }
+
 }
--- ListaTarefas/Models/Tarefa.cs
+++ ListaTarefas/Models/Tarefa.cs
@@ -1,12 +1,15 @@
-﻿namespace ListaTarefas.Models
-{
-    public class Tarefa
-    {
-        public int TarefaId { get; set; }
-        public string Nome { get; set; }
-        public Boolean Concluida { get; set; } = false;
-        public DateTime DtInicio { get; set; } = DateTime.Now;
-        public DateTime? DtConclusao { get; set; }
+﻿using System.ComponentModel.DataAnnotations;
 
-    }    
-}
+namespace ListaTarefas.Models;
+
+public class Tarefa
+{
+    public int TarefaId { get; set; }
+    public string Nome { get; set; }
+    public Boolean Concluida { get; set; } = false;
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+    public DateTime DtInicio { get; set; } = DateTime.Now;
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+    public DateTime? DtConclusao { get; set; }
+
+}    
